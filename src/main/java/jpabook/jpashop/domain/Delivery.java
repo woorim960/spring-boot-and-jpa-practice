4c55377dfca92d5051d3729de68745417b3c2ee4
woorim960 <woorimprog@gmail.com>
2022-07-06 18:13:46 +0900

    Add(domain): 도메인 추가 완료

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import static javax.persistence.FetchType.LAZY;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+  @Id @GeneratedValue
+  @Column(name = "delivery_id")
+  private Long id;
+
+  @OneToOne(mappedBy = "delivery", fetch = LAZY)
+  private Order order;
+
+  @Embedded
+  private Address address;
+
+  @Enumerated(EnumType.STRING)
+  private DeliveryStatus status; // 배송 상태 [READY, COMP] : 배송 준비. 배송
+}
--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,21 +1,21 @@
-package jpabook.jpashop;
+package jpabook.jpashop.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberRepository {
 
-  @PersistenceContext // 스프링부트가 알아서 em에 EntityManager를 주입해준다.
+  @PersistenceContext
   private EntityManager em;
 
-  public Long save(Member member) {
+  public void save(Member member) {
     em.persist(member);
-    return member.getId();
   }
 
-  public Member find(Long id) {
+  public Member findOne(Long id) {
     return em.find(Member.class, id);
   }
 }
