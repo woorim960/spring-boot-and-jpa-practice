ab92c26b687d45de2080ee4b0c80169adc862450
woorim960 <woorimprog@gmail.com>
2022-07-12 22:44:30 +0900

    Add a MemberRepository

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
@@ -17,5 +18,16 @@ public class MemberRepository {
 
   public Member findOne(Long id) {
     return em.find(Member.class, id);
+  }
+
+  public List<Member> findAll() {
+    return em.createQuery("select m from Member m",Member.class )
+        .getResultList();
+  }
+
+  public List<Member> findByName(String name) {
+    return em.createQuery("select m from Member m where m.name = :name", Member.class)
+        .setParameter("name", name)
+        .getResultList();
   }
 }
