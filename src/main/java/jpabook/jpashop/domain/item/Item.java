e4a9b00eac61cc002d0d8daabe7db06725056981
woorim960 <woorimprog@gmail.com>
2022-07-13 18:00:19 +0900

    Add a business logic for stockQuantity of Item

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,5 +36,22 @@ public abstract class Item {
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
 
+  // ==비즈니스 로직== //
+  /**
+   * stock 증가
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
 
+  /**
+   * stock 감소
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
